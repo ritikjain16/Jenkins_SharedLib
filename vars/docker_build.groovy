716e8d8fd72d119aa6427217efc7531b60171a03
ritikjain16 <72817734+ritikjain16@users.noreply.github.com>
2025-06-15 14:26:03 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 // Define function
-def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+def call(String ProjectName, String ImageTag){
+  sh "docker build -t ${ProjectName}:${ImageTag} ."
 }
